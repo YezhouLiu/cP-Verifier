1f6d3a2e57b0bd9f1248c2ea4e1a66265733dba0
Yezhou <s87117@gmail.com>
2021-11-25 11:54:58 +1300

    corrected the rule

--- test files/cpsystemtest.py
+++ test files/cpsystemtest.py
@@ -7,8 +7,7 @@ from cpparser import ParseTerms
 from cpparser import ParseRule
 import sys
 from cpverifier import CPVerifier
-
-''' 
+ 
 r1 = ParseRule('s1 f(a(b)) f(X) b ->1 s1 c g(X) d')
 sys1 = CPSystem('s1')
 sys1.AddRule(r1)
@@ -16,7 +15,6 @@ sys1.AddSystemMultiset(ParseTerms('f(a(b)) f(a(b)) f(1) b d'))
 sys1.DetailOn()
 sys1.Snapshot()
 sys1.Run()
-'''
 
 r2 = ParseRule('s1 f(1) g(1) ->+ s1 ')
 #r3 = ParseRule('s1 ->1 s2 ')
